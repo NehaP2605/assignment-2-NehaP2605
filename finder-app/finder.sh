39c2c1c6a9d45c0264bf65fd657d4ee99279f368
Neha Prabhakar <nehaprabhakar2605@gmail.com>
2025-11-28 18:57:02 +1100

    Assignment 2

---
+++ finder-app/finder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+filesdir=$1
+searchstr=$2
+if [ $# -ne 2 ]; then
+	echo " Usage $0 <filesdir> <searchstring> "
+	exit 1
+fi
+if [ ! -d "$filesdir" ]; then
+	echo " ${filesdir} directory does not exists"
+	exit 1
+fi
+
+filecount=$(find "$filesdir" -type f | wc -l)
+linecount=$(grep -r "$searchstr" "$filesdir" | wc -l)
+#output
+echo "The number of files are $filecount and the number of matching lines are $linecount"
+
